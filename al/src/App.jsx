4b467046974f9d2182a52ad9dc46f0eb0f0a24e0
adeelfeb <at4563323@gmail.com>
2024-08-26 13:19:40 +0000

    Just doingthings with a glitch i hthink so

--- al/src/App.jsx
+++ al/src/App.jsx
@@ -1,40 +1,32 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header.jsx";
-
 import Homepage from "./Components/pages/Home.jsx";
 import About from "./Components/pages/Aboutus.jsx";
 import Contact from "./Components/pages/Contact.jsx";
 import Services from "./Components/pages/Services.jsx";
-import './App.css'
-import {Route, Routes} from "react-router-dom"
+import './App.css';
 import Summarize from "./Components/pages/Summarizer/Summarize.jsx";
-
 import Modal from 'react-modal';
 
 Modal.setAppElement('#root'); // Set the app element globally
 
 function App() {
-    let components 
-
-    
     return (
         <div className="App">
-
-            <Header></Header>
-            <div className="containerForPages">
-            <Routes>
-                <Route path="/home" element={<Homepage/>}></Route>
-                <Route path="/" element={<Homepage/>}></Route>
-                <Route path="/about" element={<About/>}></Route>
-                <Route path="/services" element={<Services/>}></Route>
-                <Route path="/contact" element={<Contact/>}></Route>
-                <Route path="/summarize" element={<Summarize/>}></Route>
-            </Routes>
-            </div>
-
-
-
-            
+            <BrowserRouter>
+                <Header />
+                <div className="containerForPages">
+                    <Routes>
+                        <Route path="/home" element={<Homepage />} />
+                        <Route path="/" element={<Homepage />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/services" element={<Services />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="/summarize" element={<Summarize />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
         </div>
     );
 }
